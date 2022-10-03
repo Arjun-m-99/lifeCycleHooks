d3bc8b05f92b5715c484c56ba6f181f2631ac436
Arjun-m-99 <67136691+Arjun-m-99@users.noreply.github.com>
2022-10-03 12:55:04 +0530

    created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lifeCycleHooks';
+  name = "Angular " + VERSION.major;
+  message = "Hello";
+  content = "Hello";
+
+  constructor() {
+    console.log("AppComponent:Contructed");
+  }
+
+  ngOnChanges() {
+    console.log("AppComponent:ngOnChanges");
+  }
+
+  ngOnInit() {
+    console.log("AppComponent:ngOnInit");
+  }
+
+  ngAfterContentInit() {
+    console.log("AppComponent:ngAfterContentInit");
+  }
+
+  ngDoCheck() {
+    console.log("AppComponent:DoCheck");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AppComponent:AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AppComponent:AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AppComponent:AfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("AppComponent:ngOnDestroy");
+  }
+
 }
+
